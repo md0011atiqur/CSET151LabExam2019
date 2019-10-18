b06e63d90512b77ecefa5d6d15f7f344abc74939
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-18 17:21:27 +0600

    Not Passing exact argument will show usage style. argument aa a will show you usage

--- StudentList.java
+++ StudentList.java
@@ -4,6 +4,12 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
+		//early guard during argument pass
+		//		Check arguments
+		if(args.length != 1)
+		{
+			System.out.println("Usage: java StudentList a|r|c|?student1|+word");
+		}
 //		Check arguments
 		if(args[0].equals("a"))
 		{
