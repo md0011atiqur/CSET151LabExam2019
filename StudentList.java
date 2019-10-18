508b6836dd7ff95d2c73f3012dc5cb39ec9f1ff7
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-18 19:19:52 +0600

    remove duplicate files from code in stage-4

--- StudentList.java
+++ StudentList.java
@@ -11,23 +11,35 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt")));
 			alllines = FileStream.readLine();
-
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return alllines;
 	}
+	public  static  void WriteDate( String[] args) {
+		System.out.println("Loading data ...");
+		try {
+			BufferedWriter FileStream = new BufferedWriter(
+			new FileWriter("students.txt", true));
+			String Input = args[0].substring(1);
+			Date d = new Date();
+			String df = "dd/mm/yyyy-hh:mm:ss a";
+			DateFormat dateFormat = new SimpleDateFormat(df);
+			String fd = dateFormat.format(d);
+			FileStream.write(", " + Input + "\n List last updated on ");
+			FileStream.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("Data Loaded.");
+	}
 	//read write method optimization end
 	public static void main(String[] args) {
-
 		//early guard during argument pass
-		//		Check arguments
-		//done
 		if(args.length != 1)
 		{
 			System.out.println("Usage: java StudentList a|r|c|?student1|+word");
 		}
-		//done
 		//Check arguments
 		if(args[0].equals("a"))
 		{
@@ -37,7 +49,6 @@ public class StudentList {
 				System.out.println(word);
 				System.out.println("Data Loaded.");
 		}
-		//done
 		else if(args[0].equals("r"))
 		{
 			String alllines = LoadData();
@@ -47,27 +58,9 @@ public class StudentList {
 			System.out.println(words[y]);
 			System.out.println("Data Loaded.");
 		}
-		//processing
-		else if(args[0].contains("+"))
-		{
-			System.out.println("Loading data ...");
-			try {
-				BufferedWriter FileStream = new BufferedWriter(
-				new FileWriter("students.txt", true));
-				String Input = args[0].substring(1);
-				Date d = new Date();
-				String df = "dd/mm/yyyy-hh:mm:ss a";
-				DateFormat dateFormat = new SimpleDateFormat(df);
-				String fd = dateFormat.format(d);
-				FileStream.write(", " + Input + "\n List last updated on " + fd);
-				FileStream.close();
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-			System.out.println("Data Loaded.");
+		else if(args[0].contains("+")){
+			WriteDate(args);
 		}
-
-		//done
 		else if(args[0].contains("?"))
 		{
 			String  contents = LoadData();
@@ -79,33 +72,23 @@ public class StudentList {
 			System.out.println("Data Loaded.");
 		}
 
-		else if(args[0].contains("c"))
-		{
-			System.out.println("Loading data ...");
-			try
-			{
-				BufferedReader studentlistall = new BufferedReader(
-				new InputStreamReader(
-				new FileInputStream("students.txt")));
-				String readlines = studentlistall.readLine();
-				char charactararray[] = readlines.toCharArray();
-				boolean in_word = false;
-				int count=0;
-				for(char eachcharactar:charactararray)
-				{
-				if(eachcharactar ==' ')
-				{
-				if (!in_word) {	count++; in_word =true;	}
-				else
-					{
+		//processing
+		else if(args[0].contains("c")) {
+			String alllines = LoadData();
+			char charactararry[] = alllines.toCharArray();
+			boolean in_word = false;
+			int count=0;
+			for(char eachcharactar:charactararry) {
+				if(eachcharactar ==' ') {
+					if (!in_word) {
+						count++;
+						in_word =true;
+					} else {
 						in_word=false;
 					}
 				}
 			}
-			System.out.println(count +" word(s) found " + charactararray.length);
-			}
-			catch (Exception e)
-			{}
+			System.out.println(count +" word(s) found " + charactararry.length);
 			System.out.println("Data Loaded.");
 		}
 	}
