faedc8ec7883eb3b6794bc58c8a525a048f8ee59
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-18 22:00:53 +0600

    String literals with Constrants Replaced in stage-5

--- Constants.java
+++ Constants.java
@@ -3,10 +3,17 @@
         private Constants() {
             // restrict instantiation
         }
-        private static final String showall ="a";
-        private static final String showcount ="c";
-        private static final String showrandom ="r";
-        private static final String showsearch ="?";
-        private static final String showadd ="+";
+        public static final String showall = "a";
+        public static final String showcount ="c";
+        public static final String showrandom ="r";
+        public static final String showsearch ="?";
+        public static final String showadd ="+";
+        public static final String mainfile ="students.txt";
+        public static final String showloadingdata ="Loading data ...";
+        public static final String shwowefoundit ="We found it!";
+        public static final String showdateformat ="dd/mm/yyyy-hh:mm:ss a";
+        public static final String showusagemessage ="Usage: java StudentList a|r|c|?student1|+word";
+        public static final String showwordfound =" word(s) found ";
+
 
     }
