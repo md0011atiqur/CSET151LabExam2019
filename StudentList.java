c01bca70eaed9c33a8892404bdec4403908f5c5a
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-19 00:11:03 +0600

    more comments added to makes more reading improvements stage-10

--- StudentList.java
+++ StudentList.java
@@ -9,28 +9,35 @@ public class StudentList {
 	public  static  String LoadData() {
 		System.out.println(Constants.showloadingdata);
 		String alllines = null;
+		//alllines initialized as null
 		try {
 			BufferedReader FileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream(Constants.mainfile)));
 			alllines = FileStream.readLine();
-		} catch (Exception e) {
-			System.out.println(e);
+		}
+		catch (Exception e)
+		{
+		System.out.println(e);
 		}
 		return alllines;
 	}
+	//file write function
 	public  static  void WriteDate( String[] args) {
 		System.out.println(Constants.showloadingdata);
 		try {
 			BufferedWriter FileStream = new BufferedWriter(
-					new FileWriter(Constants.mainfile, true));
+			new FileWriter(Constants.mainfile, true));
 			String Input = args[0].substring(1);
 			Date d = new Date();
+			//date taken in d variable by using date function
 			String df = Constants.showdateformat;
 			DateFormat dateFormat = new SimpleDateFormat(df);
 			String fd = dateFormat.format(d);
 			FileStream.write(", " + Input + "\n List last updated on ");
+			//new name added in list with date and time
 			FileStream.close();
+			//after write file closed
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -47,7 +54,9 @@ public class StudentList {
 		if(args[0].equals(Constants.showall))
 		{
 			String alllines = LoadData();
+			//use loaddata method to read file
 			String words[] = alllines.split(",");
+			//split word from line by using regular expression
 			for(String word : words)
 				System.out.println(word);
 			System.out.println(Constants.showloadingdata);
@@ -55,19 +64,27 @@ public class StudentList {
 		else if(args[0].equals(Constants.showrandom))
 		{
 			String alllines = LoadData();
+			//use loaddata method to read file
 			String words[] = alllines.split(",");
+			//split word from line by using regular expression
 			Random x = new Random();
+			//random function used
 			int y = x.nextInt(1);
+			//taken single random number
 			System.out.println(words[y]);
 			System.out.println(Constants.showloadingdata);
 		}
 		else if(args[0].contains(Constants.showadd)){
 			WriteDate(args);
+		//write method use to add new student in list
 		}
+
 		else if(args[0].contains(Constants.showsearch))
 		{
 			String  contents = LoadData();
+			//use loaddata method to read file
 			String wrods[] = contents.split(",");
+			//split word from line by using regular expression
 			String Query = args[0].substring(1);
 			for(int indexlocation = 0; indexlocation<wrods.length; indexlocation++) {
 			if(wrods[indexlocation].equals(Query))
@@ -75,14 +92,15 @@ public class StudentList {
 				System.out.println(Constants.shwowefoundit);
 				break;
 			}
-
 		//processing
 		else if(args[0].contains(Constants.showcount)) {
-				String allstudentlist = LoadData();
-				String words[] = allstudentlist.split(",");
-				char charArray[] = allstudentlist.toCharArray();
-				int WordCount = words.length;
-				System.out.println(WordCount + " word(s) and " + charArray.length + " Characters Found");
-				System.out.println(Constants.shwowefoundit);
+			String allstudentlist = LoadData();
+			//use loaddata method to read file
+			String words[] = allstudentlist.split(",");
+			//split word from line by using regular expression
+			char charArray[] = allstudentlist.toCharArray();
+			int WordCount = words.length;
+			System.out.println(WordCount + " word(s) and " + charArray.length + " Characters Found");
+			System.out.println(Constants.shwowefoundit);
 	}
 }
