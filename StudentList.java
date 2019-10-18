005d1fc76ca118c9bf451a00b8638ca7da390494
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-18 22:20:35 +0600

    during mearge there is conflict. so check by only showall onbehalf of a in stage-5

--- StudentList.java
+++ StudentList.java
@@ -1,18 +1,15 @@
-
 import java.io.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
+import java.text.*;
+import java.util.*;
 public class StudentList {
 	//read write method optimization start
 	public  static  String LoadData() {
-		System.out.println(Constants.showloadingdata);
+		System.out.println("Loading data ...");
 		String alllines = null;
 		try {
 			BufferedReader FileStream = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream(Constants.mainfile)));
+							new FileInputStream("students.txt")));
 			alllines = FileStream.readLine();
 		} catch (Exception e) {
 			System.out.println(e);
@@ -20,13 +17,13 @@ public class StudentList {
 		return alllines;
 	}
 	public  static  void WriteDate( String[] args) {
-		System.out.println(Constants.showloadingdata);
+		System.out.println("Loading data ...");
 		try {
 			BufferedWriter FileStream = new BufferedWriter(
-					new FileWriter(Constants.mainfile, true));
+					new FileWriter("students.txt", true));
 			String Input = args[0].substring(1);
 			Date d = new Date();
-			String df = Constants.showdateformat;
+			String df = "dd/mm/yyyy-hh:mm:ss a";
 			DateFormat dateFormat = new SimpleDateFormat(df);
 			String fd = dateFormat.format(d);
 			FileStream.write(", " + Input + "\n List last updated on ");
@@ -34,14 +31,14 @@ public class StudentList {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		System.out.println(Constants.showloadingdata);
+		System.out.println("Data Loaded.");
 	}
 	//read write method optimization end
 	public static void main(String[] args) {
 		//early guard during argument pass
 		if(args.length != 1)
 		{
-			System.out.println(Constants.showusagemessage);
+			System.out.println("Usage: java StudentList a|r|c|?student1|+word");
 		}
 		//Check arguments
 		if(args[0].equals(Constants.showall))
@@ -50,33 +47,33 @@ public class StudentList {
 			String words[] = alllines.split(",");
 			for(String word : words)
 				System.out.println(word);
-			System.out.println(Constants.showloadingdata);
+			System.out.println("Data Loaded.");
 		}
-		else if(args[0].equals(Constants.showrandom))
+		else if(args[0].equals("r"))
 		{
 			String alllines = LoadData();
 			String words[] = alllines.split(",");
 			Random x = new Random();
 			int y = x.nextInt(1);
 			System.out.println(words[y]);
-			System.out.println(Constants.showloadingdata);
+			System.out.println("Data Loaded.");
 		}
-		else if(args[0].contains(Constants.showadd)){
+		else if(args[0].contains("+")){
 			WriteDate(args);
 		}
-		else if(args[0].contains(Constants.showsearch))
+		else if(args[0].contains("?"))
 		{
 			String  contents = LoadData();
 			String wrods[] = contents.split(",");
 			boolean done = false;
 			String Query = args[0].substring(1);
-			System.out.println(Constants.shwowefoundit);
+			System.out.println("We found it!");
 			done=true;
-			System.out.println(Constants.showloadingdata);
+			System.out.println("Data Loaded.");
 		}
 
 		//processing
-		else if(args[0].contains(Constants.showcount)) {
+		else if(args[0].contains("c")) {
 			String alllines = LoadData();
 			char charactararry[] = alllines.toCharArray();
 			boolean in_word = false;
@@ -91,8 +88,8 @@ public class StudentList {
 					}
 				}
 			}
-			System.out.println(count +Constants.showwordfound + charactararry.length);
-			System.out.println(Constants.showloadingdata);
+			System.out.println(count +" word(s) found " + charactararry.length);
+			System.out.println("Data Loaded.");
 		}
 	}
 }
